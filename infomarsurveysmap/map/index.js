37a776d642f7f71296acd5d9c1deb581e9e65031
Deirdre O'Driscoll <dodriscoll@Marine.ie>
2018-09-20 16:44:22 +0100

    updates to survey and tiles map

--- infomarsurveysmap/map/index.js
+++ infomarsurveysmap/map/index.js
@@ -14,79 +14,79 @@
 					map.setView([53.5, -8.5],7);
 		}
 		
-/* 	 $('a').click(function(e) {
-    e.preventDefault();  //stop the browser from following
-    window.location.href = 'uploads/file.doc';
-}); */
+var surveyTiles = L.tileLayer('//localhost/INFOMAR_Tiles/surveys/{z}/{x}/{y}.png', {
+            maxZoom: 14,
+            minZoom: 6,
+            opacity: 0.9,
+            attribution: '<a href="http://www.infomar.ie">INFOMAR</a>'
+        }).addTo(map);	
  
-		function popup(feature, layer) {
-			var baseLink = "//maps.marine.ie/infomarData/surveysmap/reports";
-			var execSumLink = feature.properties.ExecutiveS;
+ 
+ var surveyURL =  'https://maps.marine.ie/arcgis/rest/services/Infomar/SurveyCoverage/MapServer';
+  var surveyID = L.esri.dynamicMapLayer({url: surveyURL, layers: [0]}); 
+ 
+ 
+ map.on('click', function(e){
+	if(map.hasLayer(surveyTiles)){
+	surveyID.identify().on(map).at(e.latlng).returnGeometry(false)
+	 .run(function(error, featureCollection, response){
+		 
+		 	var baseLink = "//maps.marine.ie/infomarData/surveysmap/reports";
 			var surveyRepLink = "Explorer";
-			
-			if (feature.properties.SurveyRep !=""){
-				if (feature.properties.VESSEL_NAM == "Celtic Voyager"){
-					surveyRepLink = "Voyager";
-				}else if (feature.properties.VESSEL_NAM == "RV Keary"){
-					surveyRepLink = "Keary";
-				}
-			} 
-			
-			 var popupHTML ="<table class=\"tg\"><tr><th class=\"tg-9hbo\">Survey</th><th class=\"tg-yw4l\">" + feature.properties.SURVEY + "</th></tr><tr><td class=\"tg-9hbo\">Project</td><td class=\"tg-yw4l\">"+ feature.properties.PROJECT+ "</td></tr><tr><td class=\"tg-9hbo\">Vessel</td><td class=\"tg-yw4l\">" + feature.properties.VESSEL_NAM  +"</td></tr><tr><td class=\"tg-9hbo\">Year</td><td class=\"tg-yw4l\">"+  Math.round(feature.properties.YEAR) + "</td></tr>";
-			 
-			 if (feature.properties.ExecutiveS !='undefined' && feature.properties.ExecutiveS!=""){
-				popupHTML +=  "<tr><td class=\"tg-9hbo\">Summary Report</td><td class=\"tg-yw4l\"><a target='blank' href=\'" + baseLink + "/ExecutiveReport/"+ feature.properties.ExecutiveS+".pdf'>"+ feature.properties.SURVEY + "</a></td></tr>";
-			 }
-			 
-			 if (feature.properties.SurveyRep !='undefined' && feature.properties.SurveyRep!=""){
-				 popupHTML +=  "<tr><td class=\"tg-9hbo\">Full Report</td><td class=\"tg-yw4l\"><a target='blank' href=\'" + baseLink + "/SurveyReport/"+surveyRepLink+"/"+ feature.properties.SurveyRep+"'>"+ feature.properties.SURVEY + "</a></td></tr>";
-			 }
-			
-			
-		<!-- TO DO : Can the 'if' be removed from this function?-->
-		if (feature.properties && feature.properties.SURVEY) {
-			layer.bindPopup(popupHTML)
-			
-		}
-
-		};
+			var props = featureCollection.features["0"].properties;
 		
+		if (featureCollection.features.length > 0) { 
+			 var popupHTML ="<table class=\"tg\"><tr><th class=\"tg-9hbo\">Survey</th><th class=\"tg-yw4l\">" + props.SURVEY + "</th></tr><tr><td class=\"tg-9hbo\">Project</td><td class=\"tg-yw4l\">"+ props.PROJECT+ "</td></tr><tr><td class=\"tg-9hbo\">Vessel</td><td class=\"tg-yw4l\">" + props.VESSEL_NAM  +"</td></tr><tr><td class=\"tg-9hbo\">Year</td><td class=\"tg-yw4l\">"+  Math.round(props.YEAR) + "</td></tr>";
+			 if(props.VESSEL_NAM == "Celtic Voyager"){
+				 surveyRepLink = "Voyager";
+				}else if (props.VESSEL_NAM == "RV Keary"){
+				 surveyRepLink = "Keary";
+				}
+				
+			  if (props.ExecutiveS !='undefined' && props.ExecutiveS!=""){
+				popupHTML +=  "<tr><td class=\"tg-9hbo\">Summary Report</td><td class=\"tg-yw4l\"><a target='blank' href=\'" + baseLink + "/ExecutiveReport/"+ props.ExecutiveS+".pdf'>"+ props.SURVEY + "</a></td></tr>";
+			 }
+			  if (props.SurveyRep !='undefined' && props.SurveyRep!=""){
+				 popupHTML +=  "<tr><td class=\"tg-9hbo\">Full Report</td><td class=\"tg-yw4l\"><a target='blank' href=\'" + baseLink + "/SurveyReport/"+surveyRepLink+"/"+ props.SurveyRep+"'>"+ props.SURVEY + "</a></td></tr>";
+			 }
+	map.flyTo(e.latlng, 10,{
+			animate: true,
+			duration: 1.5
+		});
+		
+		var popupSurvey = L.popup().setLatLng(e.latlng)
+		.setContent(popupHTML)
+		.openOn(map); 
+	}	
+	});
+}
+});   
+
+		
+		var surveysOutline = L.geoJson (surveysOutline, {
+		style:  {weight: 0, fillOpacity: 0, stroke : 0},
+		simplifyFactor: 5,
+		zIndex: 1000,
+		precision: 1
+		}).addTo(map);
+
+
+		surveyTiles.on('add', (e)=>{
+			setTimeout(function(){surveysOutline.addTo(map);}, 500)
+		});
+
+			map.on('layerremove', (e)=>{
+				if (e.layer._url == '//maps.marine.ie/INFOMAR_Tiles/surveys/{z}/{x}/{y}.png'){
+				setTimeout(function(){map.removeLayer(surveysOutline);}, 500)
+			}
+		});
+				
 		function popupPlanned (feature, layer){
 			var popupHTML = "<table class=\"tg\"><tr><th class=\"tg-9hbo\">Survey</th><th class=\"tg-yw4l\">Planned Survey Area 2018</th></tr><tr><td class=\"tg-9hbo\">Survey Platform</td><td class=\"tg-yw4l\">"+ feature.properties.Vessel+ "</td></tr></table>";
 			layer.bindPopup(popupHTML);
 		};
-		
-		<!-- var to load GeoJSON in overlays -->
-		var Surveys = L.geoJson (SurveyBdrys, {
-		onEachFeature: popup,
-		style:  function(feature) {
-        switch (Math.round(feature.properties.YEAR)) {
-            case 2000: return {color: "#0000ff",'weight': 1,'opacity': 1};
-            case 2002: return {color: "#008141",'weight': 1,'opacity': 1};
-			case 2003: return {color: "#008080",'weight': 1,'opacity': 1};
-            case 2004: return {color: "#6b0021",'weight': 1,'opacity': 1};
-			case 2005: return {color: "#e86800",'weight': 1,'opacity': 1};
-			case 2007: return {color: "#a3a3a3",'weight': 1,'opacity': 1};
-            case 2006: return {color: "#b24a59",'weight': 1,'opacity': 1};
-			case 2008: return {color: "#daa520",'weight': 1,'opacity': 1};
-            case 2009: return {color: "#FF8364",'weight': 1,'opacity': 1};
-			case 2010: return {color: "#0074E4",'weight': 1,'opacity': 1};
-            case 2011: return {color: "#ffff00",'weight': 1,'opacity': 1};
-			case 2012: return {color: "#FFDD7E",'weight': 1,'opacity': 1};
-            case 2013: return {color: "#7DC383",'weight': 1,'opacity': 1};
-			case 2014: return {color: "#FFE37F",'weight': 1,'opacity': 1};
-            case 2015: return {color: "#808080",'weight': 1,'opacity': 1};
-			case 2016: return {color: "#35B0AB",'weight': 1,'opacity': 1};
-            case 2017: return {color: "#C40018",'weight': 1,'opacity': 1};
-			case 2018: return {color: "#2243B6",'weight': 1,'opacity': 1};
-        }
-    },
-		simplifyFactor: 5,
-		zIndex: 1000,
-		precision: 1,
-		//filter: mapFilter
-		}).addTo(map);
-		
+
+	
 		var plannedSurveys = L.geoJson (plannedSurveys2018, {
 		onEachFeature: popupPlanned,
 		style:  {color: "#9d00f9",'weight': 1,'opacity': 1, dashArray: '3'},
@@ -95,13 +95,15 @@
 		precision: 1
 		}).addTo(map);
 		
-			
+		var tracklines = L.esri.dynamicMapLayer({url: 'http://maps.marine.ie/arcgis/rest/services/Infomar/Tracklines/MapServer'}); 
+		
 		<!-- var to load overlays into Layer Control -->
 		var overlays = {
 		"Bathymetry" : 	bathy_Contours,
 		"Backscatter": backscatter_int,
-		"Surveys": Surveys,
-		"Planned Surveys": plannedSurveys
+		"Surveys": surveyTiles,
+		"Planned Surveys": plannedSurveys,
+		"Tracklines" : tracklines
 		};
 		
 		var baseMap = {
